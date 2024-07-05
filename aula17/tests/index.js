a31dbf2c6607e5672f5edd94b4273b0a45f4ecd0
Fábio Henrique <fabio.pauleli@gmail.com>
2024-07-04 22:25:54 -0300

    First commit

---
+++ aula17/tests/index.js
@@ -0,0 +1,35 @@
+// function soma(x = 1, y = 2) {
+//   const resultado = x + y;
+//   return resultado; // tudo que está abaixo de return, não é executado
+
+// }
+
+// const resultado = soma(5, 10);
+// console.log(resultado);
+
+// console.log(soma(2, 2));
+// console.log(soma(2, 2));
+
+
+
+
+// function saudacao(nome) {
+//   console.log(`Bom dia ${nome}!`);
+//   return 123456;
+// }
+
+// const variavel = saudacao('Fábio');
+// console.log(variavel);
+
+
+// const raiz = function (n) {  Função dentro de variavel
+//   return n ** 0.5;
+// };
+
+// console.log(raiz(9));
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
