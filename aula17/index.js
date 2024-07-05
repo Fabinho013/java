0a0d720768ddae5b3f23a23b7e67b0694e59976d
Fábio Henrique <fabio.pauleli@gmail.com>
2024-07-04 21:09:57 -0300

    First Commit

---
+++ aula17/index.js
@@ -0,0 +1,48 @@
+//            01234567890...
+// const nome = 'Fábio Henrique'
+// console.log(nome[10]);
+
+//               0       1        2             3      4
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza , Eduardo
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, 3)); Ele fatia a array e busca determiandos elementos
+// console.log(alunos.slice(0, -2)); Ele fatia no negativo.
+
+// alunos.push('Luiza');
+// alunos.push('Fábio'); Adiciona um elemento ao final da array.
+
+// alunos.unshift('Luiza');
+// alunos.unshift('Luiza'); Adiciona no começo da array
+
+// alunos.pop(); Remove o ultimo elemento da array
+// const removido = alunos.pop(); Salva o elemento que está sendo removido
+// const removido = alunos.shift(); Salva o primeito elemento que está sendo removido
+// console.log(removido);
+// console.log(alunos);
+
+// delete alunos[1]; Deleta um elemento da array e retorna undefined
+// console.log(alunos);
+
+// console.log(alunos[50]); Busca uma elemento especifico mesmo não existindo e retorna undefined
+
+
+// alunos[alunos.length] = 'Luiza';
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// console.log(alunos.length); Saber a quantidade de elementos na array
+
+// alunos[0] = 'Fábio';
+// alunos[3] = 'Luiza'; Inserir / trocar elementos do array.
+// console.log(alunos);
+
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]); Mostrar especificamente um caracter.
