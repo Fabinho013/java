7c0072aa94255fb72e167447c10938f281fdea6e
Fábio Henrique <fabio.pauleli@gmail.com>
2024-07-11 22:02:40 -0300

    First commit

---
+++ aula18/index.js
@@ -0,0 +1,27 @@
+// const array = ['1,2,3'];
+// array.push(4);
+// array[0] = 'Fábio';
+// console.log(array);
+
+// const nome01 = 'Fábio';
+// const sobrenome01 = 'Henrique';
+// const idade01 = 24;
+
+// const nome02 = 'Luiza';
+// const sobrenome02 = 'Pauleli';
+// const idade02 = 19;
+
+const pessoa1 = {
+  nome: 'Fábio',
+  sobrenome: 'Henrique',
+  idade: 24
+};
+
+const pessoa2 = {
+  nome: 'Luiza',
+  sobrenome: 'Pauleli',
+  idade: 19
+};
+
+console.log(pessoa1.nome);
+console.log(pessoa2 .nome);
